93da05fef3221476fa0a79091435c8a0bce4ab26
Natalialocatelli <natalialocatellib@gmail.com>
2022-10-17 11:23:01 -0300

    Get API

--- color generator/color generator/ViewController.swift
+++ color generator/color generator/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        makeRequest()
     }
-
-
+    
+    private func makeRequest() {
+        let url = URL(string: "https://www.thecolorapi.com/scheme")!
+        
+        let task = URLSession.shared.dataTask(with: url) {
+            (data, response, error) in
+            
+            print("response: \(String(describing: response))")
+            print("error: \(String(describing: error))")
+            
+            guard let responseData = data else {return}
+            
+            if let responseString = String(data: responseData, encoding: .utf8)
+            {
+                print(responseString)
+                
+            }
+            
+        }
+        task.resume()
+    }
 }
 
